12dba7b4d984f14c55b06187333b2b161aff81c4
Safdarali33 <78472438+Safdarali33@users.noreply.github.com>
2022-08-27 15:32:09 +0530

    Add files via upload

---
+++ attendance.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int abs = 0, late = 0;
+    string s;
+    cin >> s;
+    for (auto i = 0; i < s.size(); ++i) {
+        if (s[i] == 'A') {
+            ++abs;
+        } else if (s[i] == 'L') {
+            if (i > 0 && s[i - 1] == 'L') {
+                ++late;
+            } else {
+                late = 1;
+            }
+        }
+    }
+    if (abs > 1 || late > 2) {
+        cout<<"False";
+    }
+    else{
+        cout<<"True";
+    }
+
+    return 0;
+}
